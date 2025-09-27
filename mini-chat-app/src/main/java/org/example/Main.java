cc96c444f01e30284c1d228aff59814a6d995346
= <=>
2025-09-27 17:51:29 +0330

    add dependency of gson and write a code in main for practice

--- mini-chat-app/src/main/java/org/example/Main.java
+++ mini-chat-app/src/main/java/org/example/Main.java
@@ -1,17 +1,18 @@
 package org.example;
 
+import com.google.gson.Gson;
+import model.User;
+
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Scanner sc = new Scanner(System.in);
+        User user = User.builder().age(sc.nextInt()).name(sc.next()).userName(sc.next()).build();
+        Gson gson = new Gson();
+        String json = gson.toJson(user);
+        System.out.println(json);
     }
 }
